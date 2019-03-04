333658795bd055f92de17a0fd22fdd590864c23d
jakezp <jakezp@gmail.com>
2019-03-04 21:41:10 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 # Add config file & configure nginx
 if [[ ! -f /etc/nginx/conf.d/reverseproxy.conf ]]; then
-  echo -e "Setting up nginx"
+  echo -e "=> Setting up nginx"
   mv /nginx.conf /etc/nginx/conf.d/reverseproxy.conf
   sed -i "s/SERVERNAME/$SERVERNAME/g" /etc/nginx/conf.d/reverseproxy.conf
   sed -i "s/HOSTIP/$HOSTIP/g" /etc/nginx/conf.d/reverseproxy.conf
@@ -10,7 +10,7 @@ if [[ ! -f /etc/nginx/conf.d/reverseproxy.conf ]]; then
 fi
 
 if [[ ! -f /etc/ssl/certs/nginx/dh2048.pem ]]; then
-  echo -e "Generating DH cert"
+  echo -e "=> Generating DH cert"
   openssl dhparam -out /etc/ssl/certs/nginx/dh2048.pem 2048
 fi
 
@@ -19,8 +19,7 @@ mkdir -p /data/letsencrypt
 
 # Generate letsencrypt certificates
 if [[ ! -f /etc/letsencrypt/live/$SERVERNAME/fullchain.pem ]]; then
-  echo -e "Generating certificates..."
-  nginx -g 'daemon off;' &
+  echo -e "=> Generating certificates..."
   certbot certonly --webroot --email $EMAIL --agree-tos --no-eff-email --webroot-path=/data/letsencrypt -d $SERVERNAME
   sed -i 's/^## //g' /etc/nginx/conf.d/reverseproxy.conf
   sed -i 's/^##$//g' /etc/nginx/conf.d/reverseproxy.conf
