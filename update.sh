c8b3e3f678be712ca4319679c4151a1107935e8a
jakezp <jakezp@gmail.com>
2019-03-04 23:31:26 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-# AWS Creds
-export AWS
+# Set AWS credentials
+export AWS_ACCESS_KEY_ID=ACCESS_KEY
+export AWS_SECRET_ACCESS_KEY=SECRET_KEY
+
+# Update certificates
+certbot certonly --dns-route53
